bfdc071e4e56006279bfce1db00a335777464c2f
Reema <reemaalbrahim28@gmail.com>
2024-12-31 18:38:40 +0300

    Solving the responsiveness

--- DwellingSphere/DwellingSphere/settings.py
+++ DwellingSphere/DwellingSphere/settings.py
@@ -154,3 +154,14 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+EMAIL_BACKEND= 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST='smtp.gmail.com'
+EMAIL_PORT='587'
+EMAIL_USE_TLS= True
+EMAIL_HOST_USER=os.environ.get("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD=os.environ.get("EMAIL_HOST_PASSWORD")
+
+STATICFILES_DIRS = [
+    # 'C:\\Users\\rayoo\\Documents\\UNIT-2-PROJECT\\Personal_project\\static',  # Uncomment if needed
+]
